098a1ed75867c8e8bf6f89c0fe2dfa7410c2f5e9
Alastair Rayner <alli0@me.com>
2017-12-20 20:53:18 +0000

    Visualisations update

--- gvgai-2016/src/Visualisations/Visualisations.java
+++ gvgai-2016/src/Visualisations/Visualisations.java
@@ -7,35 +7,51 @@ import tools.Vector2d;
 
 import java.awt.*;
 import java.util.HashMap;
+import java.util.Vector;
 
 public class Visualisations
 {
-    private HashMap searchPoints = new HashMap<Vector2d, Vector2d>();
-
-    private SingleTreeNode rootNode;
+    //private HashMap searchPoints = new HashMap<Vector2d, StateObservation>();
+    private Vector<Vector2d> searchPoints = new Vector<Vector2d>();
+    private int nodesInTree = 0;
 
     public void renderSearchSpace(StateObservation SO, SingleMCTSPlayer MCTSPlayer, Graphics2D g)
     {
-        rootNode = MCTSPlayer.m_root;
-        recursivelySearchTheTree(rootNode);
+        SingleTreeNode rootNode = MCTSPlayer.m_root;
+        recursivelySearchTheTree(MCTSPlayer.m_root);
 
-        recursivelySearchTheTree(rootNode);
+        for(int i = 0; i < searchPoints.size(); i++)
+        {
+            int x = (int) searchPoints.get(i).x;
+            int y = (int) searchPoints.get(i).y;
+            g.draw3DRect((int) x, (int) y, SO.getBlockSize(), SO.getBlockSize(), false);
+        }
+        searchPoints.clear();
+        System.out.println(nodesInTree);
+        nodesInTree = 0;
+
     }
 
-    private SingleTreeNode recursivelySearchTheTree(SingleTreeNode rootNode)
+    private SingleTreeNode recursivelySearchTheTree(SingleTreeNode node)
     {
 
-        SingleTreeNode node = rootNode;
         for(int i = 0; i < node.children.length; i++)
         {
-            node = recursivelySearchTheTree(node.children[i]);
+            if(node.children[i] != null)
+            {
+                Vector2d pos = new Vector2d();
+
+                pos.x = (int) node.state.getAvatarPosition().x;
+                pos.y = (int) node.state.getAvatarPosition().y;
+
+                searchPoints.add(pos);
+                nodesInTree++;
+                node = recursivelySearchTheTree(node.children[i]);
+            }
+
         }
 
         return node;
     }
 
-    private void searchChildren(SingleTreeNode STN)
-    {
-
-    }
 }
