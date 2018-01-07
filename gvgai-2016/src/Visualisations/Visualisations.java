ade35c558903ceefe15cdd29bef77a25871ac857
Alastair Rayner <alli0@me.com>
2018-01-07 04:44:39 +0000

    Changed the colours of the visited cells based on the visits each gets

--- gvgai-2016/src/Visualisations/Visualisations.java
+++ gvgai-2016/src/Visualisations/Visualisations.java
@@ -9,6 +9,8 @@ import java.awt.*;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Vector;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class Visualisations
 {
@@ -17,7 +19,7 @@ public class Visualisations
     private Vector<Vector2d> bestPath = new Vector<Vector2d>();
     private int nodesInTree = 0;
     public boolean drawAreaSearched = true;
-    public boolean drawBestActionPath = true;
+    public boolean drawBestActionPath = false;
     private int blockOffset = 0;
 
     //! Depth search testing
@@ -31,19 +33,32 @@ public class Visualisations
         if(MCTSPlayer.m_root.state != null)
             blockOffset = MCTSPlayer.m_root.state.getBlockSize() / 2;
 
+
         // Search the tree starting at the root
         recursivelySearchTree(MCTSPlayer.m_root);
 
         // Draw the area that is being searched
         if(drawAreaSearched) {
+            for (Map.Entry<Vector2d, Integer> entry : timesPointVisited.entrySet()) {
+                Vector2d pos = entry.getKey();
+                Integer visits = entry.getValue();
+                if(visits < 3)
+                    g.setPaint(new Color(10, 100, 0));
+                if(visits >= 3 && visits <= 10)
+                    g.setPaint(new Color(242, 233, 0));
+                if(visits > 10)
+                    g.setPaint(new Color(255, 20, 58));
 
 
-
+                g.draw3DRect((int) pos.x, (int) pos.y, MCTSPlayer.m_root.state.getBlockSize(), MCTSPlayer.m_root.state.getBlockSize(), false);
+            }
+            /*
             for (int i = 0; i < searchPoints.size(); i++) {
                 int x = (int) searchPoints.get(i).x;
                 int y = (int) searchPoints.get(i).y;
                 g.draw3DRect((int) x, (int) y, MCTSPlayer.m_root.state.getBlockSize(), MCTSPlayer.m_root.state.getBlockSize(), false);
             }
+            */
 
         }
 
@@ -60,23 +75,24 @@ public class Visualisations
             Vector2d[] points = GetPathFromNode(deepestNode);
 
 
-            for (int i = 0; i < points.length; i++)
-            {
-                if(points[i] != null)
-                if (!oldPos.equals(originPoint)) {
-                    //g.setStroke(new BasicStroke(10));
-                    g.setPaint(new Color(200,100,0));
-                    g.drawLine((int) oldPos.x + blockOffset, (int) oldPos.y + blockOffset, (int) points[i].x + blockOffset, (int) points[i].y + blockOffset);
+            /*
+            for (int i = 0; i < points.length; i++) {
+                if (points[i] != null)
+                    if (!points[0].equals(originPoint)) {
+                        g.setStroke(new BasicStroke(10));
+                        g.setPaint(new Color(200, 100, 0));
+                        g.drawLine((int) oldPos.x + blockOffset, (int) oldPos.y + blockOffset, (int) points[i].x + blockOffset, (int) points[i].y + blockOffset);
 
-                }
+                    }
                 oldPos = points[i];
             }
+            */
 
 
 
 
 
-            /*
+
             // Loop through the hashmap and draw the lines between the most visited points
             for (Map.Entry<Vector2d, Integer> entry : timesPointVisited.entrySet()) {
                 Vector2d pos = entry.getKey();
@@ -86,13 +102,13 @@ public class Visualisations
                 if (!oldPos.equals(originPoint))
                 {
                     g.setStroke(new BasicStroke(visits));
-                    g.setPaint(new Color(200,100,0));
+                    g.setPaint(new Color(visits,visits / 2, visits * 2));
                     g.drawLine((int) oldPos.x + blockOffset, (int) oldPos.y+ blockOffset, (int) pos.x+ blockOffset, (int) pos.y+ blockOffset);
                 }
 
                 oldPos = pos;
             }
-            */
+
         }
 
         //System.out.println(nodesInTree + " : " + searchPoints.size());
@@ -105,7 +121,7 @@ public class Visualisations
 
         timesPointVisited.clear();
         nodesInTree = 0;
-        //deepestSearchLevel = 0;
+        deepestSearchLevel = 0;
     }
 
 
@@ -118,7 +134,8 @@ public class Visualisations
             //System.out.println(node.children.length);
             searchPoints.add(node.state.getAvatarPosition());
             nodesInTree++;
-            timesPointVisited.put(node.state.getAvatarPosition(), node.nVisits);
+
+            timesPointVisited.put(node.state.getAvatarPosition(), (int) node.totValue);
 
 
 
@@ -126,21 +143,23 @@ public class Visualisations
             SingleTreeNode depthTest = node;
             searchDepthLevel = 0;
             // If the node has a parent then check to see if it is the deepest node in the tree
-            while (depthTest != null)
+            while (depthTest.parent != null)
             {
                 if (searchDepthLevel >= deepestSearchLevel) {
                     deepestSearchLevel = searchDepthLevel;
                     deepestNode = depthTest;
-                    System.out.println(deepestSearchLevel);
+
+
+
                 }
 
-                if (depthTest.parent == null)
-                    break;
+
 
                 depthTest = depthTest.parent;
 
                 searchDepthLevel++;
             }
+
         }
 
 
@@ -159,20 +178,24 @@ public class Visualisations
     private Vector2d[] GetPathFromNode(SingleTreeNode node)
     {
         // Create an array of points to return
-        Vector2d[] res = new Vector2d[deepestSearchLevel + 1];
+        Vector2d[] res = new Vector2d[searchDepthLevel];
 
         int i = 0;
-        while (node != null)
-        {
-            if(node.state != null)
+        while (node.parent != null) {
+            if (node.state != null) {
                 res[i] = node.state.getAvatarPosition();
-            if(node.parent == null)
-                break;
-            else {
-                node = node.parent;
                 i++;
             }
+
+
+            node = node.parent;
+
+
         }
+
+        res = Arrays.stream(res)
+                .filter(s -> (s != null))
+                .toArray( Vector2d[]::new);
 
         return res;
     }
