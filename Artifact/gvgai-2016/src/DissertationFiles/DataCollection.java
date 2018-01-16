93ccda328da3b04e586afa4c1f4bba92ab4ff66e
Alastair Rayner <alli0@me.com>
2018-01-16 17:18:05 +0000

    Updated data collections json arrays and objects

--- Artifact/gvgai-2016/src/DissertationFiles/DataCollection.java
+++ Artifact/gvgai-2016/src/DissertationFiles/DataCollection.java
@@ -5,11 +5,14 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 import org.json.JSONException;
 
+import java.io.PrintWriter;
+
 //! The goal of this class is to gather the interesting data of a game
 public class DataCollection
 {
     //
-    public JSONArray AllData = new JSONArray();
+    private String outputLocation = "./gameLogs/JSONData/gameData.txt";
+    public JSONObject AllData = new JSONObject();
     public JSONArray PlayerPositions = new JSONArray();
 
     public void InitalizeDataCollection()
@@ -21,20 +24,36 @@ public class DataCollection
     public void AddGameStateToCollection(StateObservation SO)
     {
         JSONObject SOObject = new JSONObject();
+        JSONArray pos = new JSONArray();
         try
         {
-            SOObject.put("PlayerPositions", SO.getAvatarPosition());
+            SOObject.put("Pos", SO.getAvatarPosition());
             PlayerPositions.put(SOObject);
+            AllData.put("PlayerPositions", PlayerPositions);
         } catch (JSONException e)
         {
             System.out.println("Error adding to player positions");
         }
+        System.out.println(AllData.toString());
+        SaveDataToFile(AllData);
     }
 
+    //! This code writes the JSON data to a text file in the gameLogs Directory
     public void SaveDataToFile(JSONObject gameData)
     {
-        
+        try
+        {
+            PrintWriter writer = new PrintWriter(outputLocation, "UTF-8");
+            writer.println(AllData.toString());
+            writer.close();
+        }
+        catch (Exception e)
+        {
+
+        }
     }
+
+
 
 
 }
