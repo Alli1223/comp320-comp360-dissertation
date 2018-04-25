605f09b9c51aa321dfa1269155f74c94678304d4
Alastair Rayner <alli0@me.com>
2018-04-25 20:33:32 +0100

    Copied game examples to launcher directory

--- SimulationLauncher/Launcher.py
+++ SimulationLauncher/Launcher.py
@@ -7,15 +7,17 @@ def main():
     # 0: int(controller to run) 1: int(number of games) 2: int(game to run)
     simulation_count = 5000
     number_of_controllers = 5
-    java_file_location = "../Artefact/gvgai-2016/src/gvgai-2016_jar.jar"
-    execute_java(java_file_location, simulation_count, number_of_controllers)
+    number_of_concurrent_games = 5;
+    java_file_location = "../Artefact/gvgai-2016/gvgai-2016_jar.jar"
+    for x in range(0,3):
+        execute_java(java_file_location, simulation_count, number_of_controllers)
 
 
 # Execute the java program
 def execute_java(java_file, simulationCount, numberOfControllers ):
-    gvg_ai_process = subprocess.Popen(["java", "-jar", java_file], stdout=subprocess.PIPE)
+    gvg_ai_process = subprocess.Popen(["java", "-jar", java_file, "0", "1", "1"], stdout=subprocess.PIPE)
     java_returned = gvg_ai_process.stdout.read()
-    # print (java_file + java_returned)
+    print (java_file + java_returned)
 
 
 if __name__ == '__main__':
