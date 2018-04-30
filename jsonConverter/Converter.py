5d37c1363355deef4ea9c549ca9c837da5e03a93
Alastair Rayner <alli0@me.com>
2018-04-30 05:22:56 +0100

    Updated converter to process many json files at once

--- jsonConverter/Converter.py
+++ jsonConverter/Converter.py
@@ -1,39 +1,43 @@
 import csv, json, sys
 
 if sys.argv[1] is not None and sys.argv[2] is not None:
-    fileInput = sys.argv[1]
-    fileOutput = sys.argv[2]
-    inputFile = open(fileInput) # open json file
-    outputFile = open(fileOutput, 'w') # load csv file
-    data = json.load(inputFile) # load json content
-    inputFile.close() # close the input file
-    output = csv.writer(outputFile) # create a csv.write
+    for x in range(int(sys.argv[2])):
+        controller = sys.argv[1]
+        controller += "_" + str(x)
 
-    # Get the total games
-    total_games = data.keys()
-    header = dict()
-    header.update({"game": "0"})
-    header.update(data["GameData_0_0"])
+        fileInput = controller + ".txt"
+        fileOutput = controller + ".csv"
+        inputFile = open(fileInput) # open json file
+        outputFile = open(fileOutput, 'w') # load csv file
+        data = json.load(inputFile) # load json content
+        inputFile.close() # close the input file
+        output = csv.writer(outputFile) # create a csv.write
 
-    output.writerow(header)  # header row
+        # Get the total games
+        total_games = data.keys()
+        header = dict()
+        header.update({"game": "0"})
+        header.update(data["GameData_0_0"])
 
-    # Loop through the games
-    for i in range(len(total_games)):
-        gameData = []
-        game = data["GameData_0_" + str(i)]
-        TotalCellsVisisted = data["GameData_0_" + str(i)]["TotalCellsVisisted"]
-        controllerName = data["GameData_0_" + str(i)]["ControllerName"]
-        levelSize = data["GameData_0_" + str(i)]["levelSize"]
-        GameSpaceSearched = data["GameData_0_" + str(i)]["GameSpaceSearched"]
-        GameScore = data["GameData_0_" + str(i)]["GameScore"]
-        Win = data["GameData_0_" + str(i)]["Win"]
-        EndGameTick = data["GameData_0_" + str(i)]["EndGameTick"]
-        gameData.append(i)
-        gameData.append(TotalCellsVisisted)
-        gameData.append(controllerName)
-        gameData.append(levelSize)
-        gameData.append(GameSpaceSearched)
-        gameData.append(GameScore)
-        gameData.append(Win)
-        gameData.append(EndGameTick)
-        output.writerow(gameData)
+        output.writerow(header)  # header row
+
+        # Loop through the games
+        for i in range(len(total_games)):
+            gameData = []
+            game = data["GameData_0_" + str(i)]
+            TotalCellsVisisted = data["GameData_0_" + str(i)]["TotalCellsVisisted"]
+            controllerName = data["GameData_0_" + str(i)]["ControllerName"]
+            levelSize = data["GameData_0_" + str(i)]["levelSize"]
+            GameSpaceSearched = data["GameData_0_" + str(i)]["GameSpaceSearched"]
+            GameScore = data["GameData_0_" + str(i)]["GameScore"]
+            Win = data["GameData_0_" + str(i)]["Win"]
+            EndGameTick = data["GameData_0_" + str(i)]["EndGameTick"]
+            gameData.append(i)
+            gameData.append(TotalCellsVisisted)
+            gameData.append(controllerName)
+            gameData.append(levelSize)
+            gameData.append(GameSpaceSearched)
+            gameData.append(GameScore)
+            gameData.append(Win)
+            gameData.append(EndGameTick)
+            output.writerow(gameData)
